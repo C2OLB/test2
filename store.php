43bf1bd2d2ed9541bd131d0d1506310a5bc677d7
Sasha Petrenko <co2olb@gmail.com>
2018-12-16 21:46:05 +0200

    store done

--- store.php
+++ store.php
@@ -9,24 +9,41 @@
 
 
 $pdo = new PDO("mysql:host=test2; dbname=test", "root", "");
-     // добавленик  пользователя и сообщение в бд
+
+
 
 
 
 // хочу сравнить уже существует такой пользователь или нет
 
-$sql = "SELECT user_name FROM users WHERE user_name =:user_name";
-$statement = $pdo->prepare($sql);
-$statement->bindParam(':user_name',$user_name);
-$statement->execute();
-$result = $statement->fetch();
-//print_r ($result);
-echo $result[0];
+  $sql = "SELECT user_name FROM users WHERE user_name =:user_name";
+  $statement = $pdo->prepare($sql);
+  $statement->bindParam(':user_name',$user_name);
+  $statement->execute();
+  $result = $statement->fetch();
+
+
          if ($result[0] === $user_name){
-             echo "exist";
+
+             $pdo->beginTransaction();
+
+             $sql = "SELECT id FROM users WHERE user_name = :user_name";
+             $stmt = $pdo->prepare($sql);
+             $stmt->bindParam(':user_name',$user_name);
+             $stmt->execute();
+             $result1 = $stmt->fetch();
+
+
+             $sql = "INSERT INTO messages (message,message_id) VALUES (:message,$result1[0])";
+             $stmt = $pdo->prepare($sql);
+             $stmt->bindParam(':message',$message);
+             $stmt->execute();
+
+             $pdo->commit();
          }
          else{
-              $pdo->beginTransaction();
+             // добавленик  пользователя и сообщение в бд
+             $pdo->beginTransaction();
 
              $sql = "INSERT INTO users (user_name) VALUES (:user_name)";
              $stmt = $pdo->prepare($sql);
@@ -36,7 +53,6 @@ echo $result[0];
              //по последнему инсерту достаем ИД для месседжа
              $message_id = $pdo->lastInsertId();
 
-
              $sql = "INSERT INTO messages (message,message_id) VALUES (:message,$message_id)";
              $stmt = $pdo->prepare($sql);
              $stmt->bindParam(':message',$message);
@@ -44,58 +60,6 @@ echo $result[0];
 
              $pdo->commit();
          }
-   die;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- //$sql = "INSERT INTO users (user_name) VALUES (:user_name)";
- //$stmt = $pdo->prepare($sql);
- //$stmt->bindParam(':user_name',$user_name);
- //$stmt->execute();
-
-
-
-
-
-
-    //if($user_name == 0){
-         //$sql ="INSERT INTO messages (message,message_id) VALUES (:message,)";
-         //$stmt = $pdo->prepare($sql);
-         //$stmt->bindParam(':message',$message);
-         //$stmt->execute();
-
-
-   // }
-
-   // else{
-
-   // }
-
-
-
-
-
-
-
-
-
-
+         
 
 header("Location: /show.php"); exit;
